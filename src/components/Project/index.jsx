d5c63b87445ca0d524223b65deb9e7190dbbbf0c
unknown <edoyangz@gmail.com>
2024-02-15 15:56:39 +1100

    fix abit

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -19,33 +19,33 @@ const Project = () => {
       <div className="projects">
         {projects.map((project, index) => (
           <React.Fragment key={index}>
-            <Link to={`/Display/${project.title}`}>
-              <div className="project">
-                <div className="project-img">
-                  <img src={project.img || "/"} alt={project.title} />
-                  <div className="options">                    
-                    <a href={project.repo} target='_blank'>
-                      <img src='repo.svg' alt="repo" />
-                      <span className="link-text">See Repositories</span> {/* Added span here */}
-                    </a>
-                    <a href={project.link} target='_blank'>
-                      <img src='live.svg' alt="live" />
-                      <span className="link-text">See Live Demo</span> {/* And here */}
-                    </a>
-                  </div>
-                </div>
-                <div className="project-details">
-                  <h1 className="project-title">{project.title}</h1>
-                  <div className="project-side">
-                    <p className="project-date">{project.date}</p>
-                    <p className="project-tech">{project.tech}</p>
-                  </div>
-                  <p className="project-description">
-                    {renderDescriptionWithLineBreaks(project.description)}
-                  </p>
+            <div className="project">
+              <div className="project-img">
+                <img src={project.img || "/"} alt={project.title} />
+                <div className="options">                    
+                  <a href={project.repo} target='_blank'>
+                    <img src='repo.svg' alt="repo" />
+                    <span className="link-text">See Repositories</span> {/* Added span here */}
+                  </a>
+                  <a href={project.link} target='_blank'>
+                    <img src='live.svg' alt="live" />
+                    <span className="link-text">See Live Demo</span> {/* And here */}
+                  </a>
                 </div>
               </div>
-            </Link>
+                <Link to={`/Display/${project.title}`}>
+                  <div className="project-details">
+                    <h1 className="project-title">{project.title}</h1>
+                    <div className="project-side">
+                      <p className="project-date">{project.date}</p>
+                      <p className="project-tech">{project.tech}</p>
+                    </div>
+                    <p className="project-description">
+                      {renderDescriptionWithLineBreaks(project.description)}
+                    </p>
+                  </div>
+                </Link>
+            </div>
             {index < projects.length - 1 && <hr />}
           </React.Fragment>
         ))}
