cbdfd729b4501e9b94e7ee170861e34fdedac9d6
unknown <edoyangz@gmail.com>
2024-11-01 23:10:00 +1100

    configuration

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,18 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Link } from 'react-router-dom';
-import './style.scss';
-import useTypingEffect from '../../utils/TypingEffect';
-import { useTheme } from '../../utils/DarkMode';
+import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import "./style.scss";
+import useTypingEffect from "../../utils/TypingEffect";
+import { useTheme } from "../../utils/DarkMode";
 
 const Header = () => {
   const [isActive, setIsActive] = useState(false);
-  const words = ['Web Developer', 'Fullstack Developer', 'UI/UX Developer'];
+  const words = ["Web Developer", "Fullstack Developer", "UI/UX Developer"];
   const { text, cursorVisible } = useTypingEffect(words, 100, 50);
   const { theme, toggleTheme } = useTheme();
   const headerRef = useRef(null); // Ref for the header element
 
   const themeIconStyle = {
-    transform: theme === 'dark' ? 'rotate(360deg)' : 'rotate(0deg)',
+    transform: theme === "dark" ? "rotate(360deg)" : "rotate(0deg)",
   };
 
   const toggleHamburger = () => {
@@ -29,28 +29,43 @@ const Header = () => {
   // useEffect to add and remove the event listener
   useEffect(() => {
     if (isActive) {
-      document.addEventListener('click', handleClickOutside);
+      document.addEventListener("click", handleClickOutside);
     } else {
-      document.removeEventListener('click', handleClickOutside);
+      document.removeEventListener("click", handleClickOutside);
     }
     // Cleanup function to remove event listener
-    return () => document.removeEventListener('click', handleClickOutside);
+    return () => document.removeEventListener("click", handleClickOutside);
   }, [isActive]); // Re-bind event listener if isActive changes
 
   return (
-    <div className='header' ref={headerRef}>
+    <div className="header" ref={headerRef}>
       <div className="logo">
-        <Link to="/"><p>{text}<span className={`cursor ${cursorVisible ? 'blink' : ''}`}>|</span></p></Link>
+        <Link to="/">
+          <p>
+            {text}
+            <span className={`cursor ${cursorVisible ? "blink" : ""}`}>|</span>
+          </p>
+        </Link>
       </div>
-      <div className={`menu ${isActive ? 'active' : ''}`}>
+      <div className={`menu ${isActive ? "active" : ""}`}>
         <Link to="/">Home</Link>
-        <Link to="/project">Projects</Link> 
+        <Link to="/project">Projects</Link>
         <Link to="/blog">Blog</Link>
-        <div className="theme-switcher" style={themeIconStyle} onClick={toggleTheme}>
-          <img src={theme === 'light' ? 'light-mode.svg' : 'dark-mode.svg'} alt="theme"/>
+        <div
+          className="theme-switcher"
+          style={themeIconStyle}
+          onClick={toggleTheme}
+        >
+          <img
+            src={theme === "light" ? "light-mode.svg" : "dark-mode.svg"}
+            alt="theme"
+          />
         </div>
       </div>
-      <div className={`hamburger ${isActive ? 'active' : ''}`} onClick={toggleHamburger}>
+      <div
+        className={`hamburger ${isActive ? "active" : ""}`}
+        onClick={toggleHamburger}
+      >
         <span></span>
         <span></span>
         <span></span>
