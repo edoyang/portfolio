8cb47de278b396d36105e934c10496901f99bc40
unknown <edoyangz@gmail.com>
2024-11-04 16:40:20 +1100

    update again

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -17,38 +17,40 @@ const Project = () => {
     <div className="padding-lg">
       <h2 className="font-title">Projects</h2>
       <div className="projects">
-        {projects.map((project, index) => (
-          <React.Fragment key={index}>
-            <div className="project">
-              <div className="project-img">
-                <img src={project.img || "/"} alt={project.title} />
-                <div className="options">
-                  <a href={project.repo} target="_blank">
-                    <img src="repo.svg" alt="repo" />
-                    <span className="link-text">See Repositories</span>
-                  </a>
-                  <a href={project.link} target="_blank">
-                    <img src="live.svg" alt="live" />
-                    <span className="link-text">See Live Demo</span>
-                  </a>
-                </div>
-              </div>
-              <Link to={`/display-project/${project.id}`}>
-                <div className="project-details">
-                  <h1 className="project-title">{project.title}</h1>
-                  <div className="project-side">
-                    <p className="project-date">{project.date}</p>
-                    <p className="project-tech">{project.tech}</p>
+        {projects
+          .sort((a, b) => b.id - a.id)
+          .map((project, index) => (
+            <React.Fragment key={index}>
+              <div className="project">
+                <div className="project-img">
+                  <img src={project.img || "/"} alt={project.title} />
+                  <div className="options">
+                    <a href={project.repo} target="_blank">
+                      <img src="repo.svg" alt="repo" />
+                      <span className="link-text">See Repositories</span>
+                    </a>
+                    <a href={project.link} target="_blank">
+                      <img src="live.svg" alt="live" />
+                      <span className="link-text">See Live Demo</span>
+                    </a>
                   </div>
-                  <p className="project-description">
-                    {renderDescriptionWithLineBreaks(project.description)}
-                  </p>
                 </div>
-              </Link>
-            </div>
-            {index < projects.length - 1 && <hr />}
-          </React.Fragment>
-        ))}
+                <Link to={`/display-project/${project.id}`}>
+                  <div className="project-details">
+                    <h1 className="project-title">{project.title}</h1>
+                    <div className="project-side">
+                      <p className="project-date">{project.date}</p>
+                      <p className="project-tech">{project.tech}</p>
+                    </div>
+                    <p className="project-description">
+                      {renderDescriptionWithLineBreaks(project.description)}
+                    </p>
+                  </div>
+                </Link>
+              </div>
+              {index < projects.length - 1 && <hr />}
+            </React.Fragment>
+          ))}
       </div>
     </div>
   );
